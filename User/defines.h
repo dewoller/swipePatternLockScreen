d89b511b4a1d3c68dff2eb30245c52ab21b7fbd4
dewoller <dewoller@gmail.com>
2014-09-03 20:05:23 +1000

    fixed USB output

--- User/defines.h
+++ User/defines.h
@@ -1,14 +1,14 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
-//#define TM_DISCO_STM32F429_DISCOVERY
+#define TM_DISCO_STM32F429_DISCOVERY
 
 /* Activate USB HS in FS mode */
-//#define USE_USB_OTG_HS
+#define USE_USB_OTG_HS
 #include "stm32f4xx.h"
 
 //Change SPI5 baudrate prescaler APB2 / 2
 #define TM_SPI5_PRESCALER	SPI_BaudRatePrescaler_2
-	#define TM_ILI9341_BUTTON_MAX_BUTTONS	13
+#define TM_ILI9341_BUTTON_MAX_BUTTONS	13
 
 #endif 
